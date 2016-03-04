f7990cb7ddbcf756d0e61755e6464bf6edc6562b
sovanlandy2 <sovanlandy@gmail.com>
2016-03-03 23:24:03 -0800

    ok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
-    :s3_host_name: "leocup-production.s3.amazonaws.com"
+    :s3_host_name => 'leocup-production.s3.amazonaws.com'
   }
 
 end
