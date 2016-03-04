bde862ec1db4bc9a037c2659c25b241390f0b7a3
sovanlandy2 <sovanlandy@gmail.com>
2016-03-03 22:31:01 -0800

    ok

--- app/assets/javascripts/visitors.js
+++ app/assets/javascripts/visitors.js
@@ -1,13 +1,3 @@
 $(document).ready(function() {
-    $('#myCarousel').carousel({interval: 2000});
-    $('#myCarousel').on('slid', function() {
-        var to_slide = $('.carousel-item.active').attr('data-slide-no');
-        $('.myCarousel-target.active').removeClass('active');
-        $('.carousel-indicators [data-slide-to=' + to_slide + ']').addClass('active');
-    });
-    $('.myCarousel-target').on('click', function() {
-        $('#myCarousel').carousel(parseInt($(this).attr('data-slide-to')));
-        $('.myCarousel-target.active').removeClass('active');
-        $(this).addClass('active');
-    });
+    $('#myCarousel').carousel({interval: 5000});
 });
