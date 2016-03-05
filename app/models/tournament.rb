5d1a778d768d4a22fba09f3cafe9523ba2724730
sovanlandy2 <sovanlandy@gmail.com>
2016-03-05 01:28:45 -0800

    ok

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -8,15 +8,17 @@ class Tournament < ActiveRecord::Base
 	accepts_nested_attributes_for :pools
 
 	def generate_matches
-		if pools.size == 2
+		if self.pools.count == 2
 			# first pool
-			poolA = pools.first
-			poolB = pools.last
-			size = poolA.size
+			poolA = self.pools.first
+			poolB = self.pools.last
+			teamsA = poolA.teams.to_a 
+			teamsB = poolB.teams.to_a 
+			size = teamsA.size
 			(0..size-1).each do |l|
 				(1..size-1).each do |j|
 					if (l != j)
-						match = Match.where(team_left_id: poolA[l].id, team_right_id: poolA[j].id, tournament_id: self.id).first_or_create(score: ["0-0", "0-0", "0-0", "0-0"], stage: poolA.name)
+						match = Match.where(team_left_id: teamsA[l].id, team_right_id: teamsA[j].id, tournament_id: self.id).first_or_create(score: ["0-0", "0-0", "0-0", "0-0"], stage: poolA.name)
 						puts "Create a match: " 
 						puts match.attributes
 					end
@@ -26,7 +28,7 @@ class Tournament < ActiveRecord::Base
 			(0..size-1).each do |l|
 				(1..size-1).each do |j|
 					if (l != j)
-						match = Match.where(team_left_id: poolB[l].id, team_right_id: poolB[j].id, tournament_id: self.id).first_or_create(score: ["0-0", "0-0", "0-0", "0-0"], stage: poolA.name)
+						match = Match.where(team_left_id: teamsB[l].id, team_right_id: teamsB[j].id, tournament_id: self.id).first_or_create(score: ["0-0", "0-0", "0-0", "0-0"], stage: poolB.name)
 						puts "Create a match: " 
 						puts match.attributes
 					end
