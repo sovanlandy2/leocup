428a111cfde7dd8a92443c0c3ed94216ae63247d
sovanlandy2 <sovanlandy@gmail.com>
2016-03-08 21:59:35 -0800

    ok

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,8 +1,8 @@
 class VisitorsController < ApplicationController
 	def index
-		@matches = Match.order("match_date desc nulls last").limit(5)
+		@matches = Match.where(is_completed:true).order("match_date desc nulls last").limit(5)
 		@voted_teams = Team.all.last(10)
-		@current_tournament = Tournament.first
+		@current_tournament = Tournament.where(is_current_tournament: true).first || Tournament.first
 		@gallery_photos = GalleryPhoto.order("created_at desc").limit(30)
 	end
 end
