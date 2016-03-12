973226516fb3ae28963853751f4752ab905c50c8
sovanlandy2 <sovanlandy@gmail.com>
2016-03-11 18:19:08 -0800

    ok

--- db/migrate/20160312003927_translate_news.rb
+++ db/migrate/20160312003927_translate_news.rb
@@ -1,4 +1,15 @@
 class TranslateNews < ActiveRecord::Migration
-  def change
+  def self.up
+    Post.create_translation_table!({
+      :title => :string,
+      :content => :text,
+      :source => :string
+    }, {
+      :migrate_data => true
+    })
+  end
+
+  def self.down
+    Post.drop_translation_table! :migrate_data => true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311235251) do
+ActiveRecord::Schema.define(version: 20160312003927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -167,6 +167,19 @@ ActiveRecord::Schema.define(version: 20160311235251) do
   end
 
   add_index "post_photos", ["post_id"], name: "index_post_photos_on_post_id", using: :btree
+
+  create_table "post_translations", force: :cascade do |t|
+    t.integer  "post_id",    null: false
+    t.string   "locale",     null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "title"
+    t.text     "content"
+    t.string   "source"
+  end
+
+  add_index "post_translations", ["locale"], name: "index_post_translations_on_locale", using: :btree
+  add_index "post_translations", ["post_id"], name: "index_post_translations_on_post_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
