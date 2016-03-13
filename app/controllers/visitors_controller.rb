c2c798ad633e46f40707320005a29018f09968d9
sovanlandy2 <sovanlandy@gmail.com>
2016-03-13 01:20:08 -0800

    ok

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -9,5 +9,8 @@ class VisitorsController < ApplicationController
 										.includes(:pools, :matches , :team_pools)
 										.current_tournaments.first || Tournament.first
 		@gallery_photos = GalleryPhoto.order("created_at desc").limit(30)
+
+		image_url = @gallery_photos.first.photo.url rescue nil
+		set_meta_tags og: { image: image_url}
 	end
 end
