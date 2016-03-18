03d0eb167aab020a068ce73012356c4d2bff8201
sovanlandy2 <sovanlandy@gmail.com>
2016-03-17 23:34:01 -0700

    resize image in fb

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -19,7 +19,7 @@ class TeamsController < ApplicationController
 		@current_coach = @current_team.coach
 		@gallery_photos = @current_team.team_gallery_photos
 
-		image_url = @gallery_photos.first.photo.url rescue nil
+		image_url = @gallery_photos.first.photo.url(:thumb) rescue nil
 		set_meta_tags og: { image: image_url, title: @current_team.name, description: @current_team.description}
 		render :index
 	end
