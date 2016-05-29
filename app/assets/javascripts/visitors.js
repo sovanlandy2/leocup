e04924098c4c1c58c606b5883215afcc5990e6f9
sovanlandy <sovanlandy@gmail.com>
2016-05-28 21:47:03 -0700

    only can vote once and 5 team max

--- app/assets/javascripts/visitors.js
+++ app/assets/javascripts/visitors.js
@@ -6,6 +6,17 @@ $(document).ready(function() {
 
     $('.vote_team').click(function () {    
     	$(this).toggleClass('active');
+        var arrayId = [];
+        $('.vote_team.active').each( function () {
+            var id = $(this).attr('id').split('voteteam')[1];
+            arrayId.push(id);
+        });
+        var team_ids = arrayId.join(",");
+        if (arrayId.length > 5){
+            $('#save_changes').hide();
+        } else {
+            $('#save_changes').show();
+        }
     });
 
     $('#save_changes').click(function (e) {
@@ -15,7 +26,13 @@ $(document).ready(function() {
             arrayId.push(id);
         });
         var team_ids = arrayId.join(",");
-        $.post("/team_votes?team_ids="+team_ids);
-        $('#vote_saved').show();
+        if (arrayId.length<=5){
+            $.post("/team_votes?team_ids="+team_ids);
+            $('#vote_saved').show();
+        }
+        
     });
+
+
+
 });
