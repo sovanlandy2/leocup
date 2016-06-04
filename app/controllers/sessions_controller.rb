b89b6310982a8ecb7f3a2b5e9514c8206baa2577
sovanlandy <sovanlandy@gmail.com>
2016-06-03 18:32:34 -0700

    update login message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,8 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    flash[:msg] = "You have logged in successfully, please click on team name or 'Start Voting' again to vote"
+    flash[:msg] = "លោកអ្នកបានជ្រើសរើសយកក្រុមប្រកួតដូចខាងក្រោម
+ប្រសិនបើក្រុមណាមួយក្លាយជាម្ចាស់ជើងឯក LEO CUP NATIONAL VOLLEYBALL 2016"
     redirect_to root_url+"#all_voting"
   end
 
