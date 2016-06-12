b2630e10628f1a03828a5c46da92331df251c148
Sovanlandy Pheng <sovanlan@amazon.com>
2016-06-12 13:23:04 -0700

    update vote

--- app/controllers/team_votes_controller.rb
+++ app/controllers/team_votes_controller.rb
@@ -8,10 +8,13 @@ class TeamVotesController < ApplicationController
 		to_delete_ids = existing_team_ids - requests_team_ids
 		to_create_ids = requests_team_ids - existing_team_ids
 
+		current_tournament = Tournament.visible
+									   .current_tournaments.first || Tournament.first
+
 		TeamVote.where(user:user, team_id: to_delete_ids).destroy_all
 		
 		to_create_ids.each do |id|
-			TeamVote.where(user:user, team_id: id).first_or_create
+			TeamVote.where(user:user, team_id: id, tournament_id: current_tournament.id).first_or_create
 		end
 		redirect_to root_url+"#all_voting"
 	end	
