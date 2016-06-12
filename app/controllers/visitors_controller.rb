be5e303d17849c284f9847acede88caeae811fb1
Sovanlandy Pheng <sovanlan@amazon.com>
2016-06-12 11:31:06 -0700

    update controller

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -10,7 +10,7 @@ class VisitorsController < ApplicationController
 										.current_tournaments.first || Tournament.first
 		@gallery_photos = GalleryPhoto.order("created_at desc").limit(16)
 		@winning_teams_raw = Team.where(id: @current_tournament.team_pools.pluck(:team_id))
-		@winning_teams = @winning_teams_raw.to_a.each_slice(5).to_a
+		@winning_teams = @winning_teams_raw.to_a.each_slice(4).to_a
 		@total_votes = TeamVote.count
 		if current_user.present?
 			@current_voted_teams = TeamVote.where(user_id: current_user.id).pluck(:team_id)
