7eebb7d429e3d0e4a5a190ac8aa10507bfe605c5
Sovanlandy Pheng <sovanlan@amazon.com>
2016-06-12 13:39:36 -0700

    update vote

--- app/controllers/team_votes_controller.rb
+++ app/controllers/team_votes_controller.rb
@@ -3,13 +3,14 @@ class TeamVotesController < ApplicationController
 	def create
 		user = current_user
 		return 		redirect_to root_url+"#all_voting" if !user.present? || !params[:team_ids].present?
-		requests_team_ids = params[:team_ids].split(",").map{|r| r.to_i}.sort
-		existing_team_ids = TeamVote.where(user: user).pluck(:team_id).sort
-		to_delete_ids = existing_team_ids - requests_team_ids
-		to_create_ids = requests_team_ids - existing_team_ids
-
 		current_tournament = Tournament.visible
 									   .current_tournaments.first || Tournament.first
+		requests_team_ids = params[:team_ids].split(",").map{|r| r.to_i}.sort
+		existing_team_ids = TeamVote.where(user: user, tournament_id: current_tournament.id)
+									.pluck(:team_id)
+									.sort
+		to_delete_ids = existing_team_ids - requests_team_ids
+		to_create_ids = requests_team_ids - existing_team_ids
 
 		TeamVote.where(user:user, team_id: to_delete_ids).destroy_all
 		
